10bd377c0d47b3239b4aaaff17abad860ccdc88a
zoroqi <zhangqi560832@gmail.com>
2025-01-08 16:35:59 +0800

    1.5 task 1

--- mini-lsm-starter/src/iterators/two_merge_iterator.rs
+++ mini-lsm-starter/src/iterators/two_merge_iterator.rs
@@ -1,9 +1,8 @@
 #![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
-use anyhow::Result;
-
 use super::StorageIterator;
+use anyhow::Result;
 
 /// Merges two iterators of different types into one. If the two iterators have the same key, only
 /// produce the key once and prefer the entry from A.
@@ -11,6 +10,7 @@ pub struct TwoMergeIterator<A: StorageIterator, B: StorageIterator> {
     a: A,
     b: B,
     // Add fields as need
+    choose_a: bool,
 }
 
 impl<
@@ -19,8 +19,45 @@ impl<
     > TwoMergeIterator<A, B>
 {
     pub fn create(a: A, b: B) -> Result<Self> {
-        unimplemented!()
+        let mut iter = TwoMergeIterator { a, b, choose_a: true };
+        iter.choose()?;
+        Ok(iter)
     }
+    fn choose(&mut self) -> Result<()> {
+        match (self.a.is_valid(), self.b.is_valid()) {
+            (false, true) => {
+                self.choose_a = false;
+            }
+            (true, false) => {
+                self.choose_a = true;
+            }
+            (true, true) => {
+                let cmp = self.a.key().cmp(&self.b.key());
+                match cmp {
+                    std::cmp::Ordering::Less => {
+                        self.choose_a = true;
+                    }
+                    std::cmp::Ordering::Greater => {
+                        self.choose_a = false;
+                    }
+                    std::cmp::Ordering::Equal => {
+                        self.b.next()?;
+                        self.choose_a = true;
+                    }
+                }
+            }
+            _ => {}
+        }
+        Ok(())
+    }
+    // 如何实现这么一个函数? current_iter()时, 返回当前的迭代器, 方便在执行 key, value, is_valid 代码更简单一些.
+    // fn current_iter(&self) -> impl StorageIterator {
+    //     if self.a_or_b {
+    //         &self.a
+    //     } else {
+    //         &self.b
+    //     }
+    // }
 }
 
 impl<
@@ -31,18 +68,35 @@ impl<
     type KeyType<'a> = A::KeyType<'a>;
 
     fn key(&self) -> Self::KeyType<'_> {
-        unimplemented!()
+        if self.choose_a {
+            self.a.key()
+        } else {
+            self.b.key()
+        }
     }
 
     fn value(&self) -> &[u8] {
-        unimplemented!()
+        if self.choose_a {
+            self.a.value()
+        } else {
+            self.b.value()
+        }
     }
 
     fn is_valid(&self) -> bool {
-        unimplemented!()
+        if self.choose_a {
+            self.a.is_valid()
+        } else {
+            self.b.is_valid()
+        }
     }
 
     fn next(&mut self) -> Result<()> {
-        unimplemented!()
+        if self.choose_a {
+            self.a.next()?;
+        } else {
+            self.b.next()?;
+        }
+        self.choose()
     }
 }
