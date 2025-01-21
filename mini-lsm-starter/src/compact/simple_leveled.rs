674a780683a5b166402551192d37b1c57920a15e
zoroqi <zhangqi560832@gmail.com>
2025-01-21 17:44:21 +0800

    2.2 Task 1: Simple Leveled Compaction

--- mini-lsm-starter/src/compact/simple_leveled.rs
+++ mini-lsm-starter/src/compact/simple_leveled.rs
@@ -1,6 +1,6 @@
-use serde::{Deserialize, Serialize};
-
 use crate::lsm_storage::LsmStorageState;
+use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
 
 #[derive(Debug, Clone)]
 pub struct SimpleLeveledCompactionOptions {
@@ -35,7 +35,36 @@ impl SimpleLeveledCompactionController {
         &self,
         _snapshot: &LsmStorageState,
     ) -> Option<SimpleLeveledCompactionTask> {
-        unimplemented!()
+        if self.options.max_levels == 0 {
+            return None;
+        }
+        println!("l0_sstables_len {}, max:{}", _snapshot.l0_sstables.len(), self.options.level0_file_num_compaction_trigger);
+        let l0 = _snapshot.l0_sstables.len();
+        if l0 >= self.options.level0_file_num_compaction_trigger {
+            return Some(SimpleLeveledCompactionTask {
+                upper_level: None,
+                upper_level_sst_ids: _snapshot.l0_sstables.clone(),
+                lower_level: 1,
+                lower_level_sst_ids: _snapshot.levels[0].1.clone(),
+                is_lower_level_bottom_level: false,
+            });
+        }
+        for i in 0..self.options.max_levels - 1 {
+            let l_upper = _snapshot.levels[i].1.len();
+            let l_lower = _snapshot.levels[i + 1].1.len();
+            let ratio = (l_lower as f64 / l_upper as f64) * 100.0;
+            println!("l_upper {} l_lower {} ratio {} size_ratio {}", l_upper, l_lower, ratio, self.options.size_ratio_percent);
+            if ratio < self.options.size_ratio_percent as f64{
+                return Some(SimpleLeveledCompactionTask {
+                    upper_level: Some(i+1),
+                    upper_level_sst_ids: _snapshot.levels[i].1.clone(),
+                    lower_level: i + 2,
+                    lower_level_sst_ids: _snapshot.levels[i + 1].1.clone(),
+                    is_lower_level_bottom_level: l_upper == self.options.max_levels-1,
+                });
+            }
+        }
+        None
     }
 
     /// Apply the compaction result.
@@ -51,6 +80,24 @@ impl SimpleLeveledCompactionController {
         _task: &SimpleLeveledCompactionTask,
         _output: &[usize],
     ) -> (LsmStorageState, Vec<usize>) {
-        unimplemented!()
+        let mut snapshot = _snapshot.clone();
+        let mut remove_sst_ids = Vec::new();
+        if _task.upper_level.is_none() {
+            remove_sst_ids.extend(_task.upper_level_sst_ids.clone());
+            let mut l0_sstables_map = snapshot.l0_sstables.iter().copied().collect::<HashSet<_>>();
+            snapshot.l0_sstables = snapshot
+                .l0_sstables
+                .iter()
+                .filter(|x| !l0_sstables_map.remove(x))
+                .copied()
+                .collect::<Vec<_>>();
+        } else {
+            let upper = _task.upper_level.unwrap() - 1;
+            remove_sst_ids.extend(_task.upper_level_sst_ids.clone());
+            snapshot.levels[upper].1.clear();
+        }
+        remove_sst_ids.extend(_task.lower_level_sst_ids.clone());
+        snapshot.levels[_task.lower_level - 1].1 = _output.to_vec();
+        (snapshot, remove_sst_ids)
     }
 }
